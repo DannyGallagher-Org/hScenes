098c2fce06379468024b6428cde2c6d48dbf8443
DannyG59 <danielgallagher.59@gmail.com>
2023-06-18 10:51:24 +0100

    fix package

---
+++ Code/Levels/AdditiveScene.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using hUtility.ScriptableVariables.Strings;
+using SerializableDictionary.Implementations;
+using UnityEngine;
+
+namespace Levels
+{
+    [CreateAssetMenu(fileName = "NewSceneObject", menuName = "How/Scene")]
+    [Serializable]
+    public class AdditiveScene : ScriptableObject
+    {
+        public StringReference Name = new StringReference();
+        public StringReference LightingScene = new StringReference();
+
+        [SerializeField]
+        private SDStringString _optionalLayers;
+        public SDStringString OptionalLayers => _optionalLayers;
+
+        [SerializeField]
+        [HideInInspector]
+        public string[] Layers = {};
+
+        [SerializeField]
+        private SDStringString _stateLayers;
+        public SDStringString StateLayers => _stateLayers;
+
+        public AdditiveScene()
+        {
+            _optionalLayers = new SDStringString();
+        }
+    }
+}
